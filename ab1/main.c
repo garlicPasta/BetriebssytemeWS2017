9da77e992aa00785d96005f8ef3e2b4a433907f6
Jakob Krause <jakobkrause.mail@gmail.com>
2017-11-09 00:39:46 +0100

    got rid of warninigs

--- ab1/main.c
+++ ab1/main.c
@@ -7,28 +7,30 @@
 #include <stdarg.h>
 
 
+volatile char * const TRANS_ADDR = (char *) DEBUG_REG + WRITE_OFF;
+volatile char * const STATUS_ADDR = (char *) DEBUG_REG + STATUS_OFF;
+volatile char * const RECEIV_ADDR = (char *) DEBUG_REG + RECEIV_OFF;
+
 static inline void print_c(char c){
-    volatile unsigned char * UART0DR = (unsigned int *) DEBUG_REG;
-    UART0DR = UART0DR + WRITE_OFF;
-    *UART0DR = c; // schreibe char
+    *TRANS_ADDR = c;                           // schreibe char
 }
 
 
 static inline void print_s(const char *s){
-    while(*s != '\0') { // Iteriere durch den String
+    while(*s != '\0') {                     // Iteriere durch den String
         print_c(*s);
-        s++; // naechster Buchstabe
+        s++;                                // naechster Buchstabe
     }
 }
 
 static inline void print_x_help(char b){
-    char c = '?'; // default
+    char c = '?';                           // default
 
     if( b<10){
-        c = '0' +b; // Zahl [0-9] -> [0-9]
+        c = '0' +b;                         // Zahl [0-9] -> [0-9]
     }
     if(10<= b && b<16){
-        c = 'A' +b -10; // Buchstabe [10-15] -> [A-F]
+        c = 'A' +b -10;                     // Buchstabe [10-15] -> [A-F]
     }
     print_c(c);
 }
@@ -37,18 +39,18 @@ static inline void print_x(unsigned int x){
     int i;
     int bendian[] = {0,1,2,3};
 
-    int *endianOrder = bendian; // unterschiedliche Anordnung der Bytes
-    char* pre= "0x"; // fancy
+    int *endianOrder = bendian;             // unterschiedliche Anordnung der Bytes
+    char* pre= "0x";
     print_s(pre);
 
-    char* p = (char*) &x; // int = 4 bytes .... byte array
+    char* p = (char*) &x;                   // int = 4 bytes .... byte array
     char b= 0;
 
     for(i=0; i<INT_BYTE_COUNT; i++){
         b = (p[endianOrder[i]]) & 0xF0; 	// nimm forderen 4 Bits
         b = b>>4;		                    // schiebe sie and den anfang(rechts)
         print_x_help(b);
-        b = (p[endianOrder[i]]) & 0x0F;	// nimm hintere 4 Bits
+        b = (p[endianOrder[i]]) & 0x0F;	    // nimm hintere 4 Bits
         print_x_help(b);
     }
 }
@@ -58,58 +60,39 @@ static inline void print_p(void* p){
     print_x(x);
 }
 
-static inline int count_params_from(const char *format_string){
-    int count =0;
-    while(*format_string != '\0') {
-        if(*format_string == '%'){count++;}
-        format_string++;
-    }
-    return count;
-}
-
-
 void my_print_f(const char *format_string,...) {
     va_list args;
-    int num_args = count_params_from(format_string); // N der Argumente notwendig
-    va_start(args,num_args); // nun kann va_arg benutzt werden
-
-    // Format String durchlaufen und gegebenfalls Argumente einbinden
-    while(*format_string != '\0') { // Null terminierte Strings
+    va_start(args,format_string);                       // nun kann va_arg benutzt werden
+                                                        // Format String durchlaufen und gegebenfalls Argumente einbinden
+    while(*format_string != '\0') {                     // Null terminierte Strings
         if(*format_string == '%'){
-            format_string++; // falls dann muss nun ein Zeichen fÃ¼r das Format kommen
-            switch((*format_string)){
-                // Nutze entsprechende Print funktion
+            format_string++;                            // falls dann muss nun ein Zeichen fÃ¼r das Format kommen
+            switch((*format_string)){                   // Nutze entsprechende Print funktion
                 case  'c':
-                    print_c((char)va_arg(args,int));
+                    print_c((char) va_arg(args, int));
                     break;
                 case  's':
-                    print_s(va_arg(args,const char*));
+                    print_s(va_arg(args, const char*));
                     break;
                 case  'x':
-                    print_x(va_arg(args,unsigned int));
+                    print_x(va_arg(args, unsigned int));
                     break;
                 case  'p':
-                    print_p(va_arg(args,void*));
+                    print_p(va_arg(args, void*));
                     break;
                 case '\0':
-                    continue; // Error im naechsten schritt wird Print beendet
+                    continue;                               // Error im naechsten schritt wird Print beendet
                 default:
-                    // war einfach nur ein Prozent Zeichen
-                    print_c(*format_string);
+                    print_c(*format_string);                // war einfach nur ein Prozent Zeichen
             }
         }else{
             print_c(*format_string);
         }
-        format_string++;// pointer auf das naechste Zeichen setzen
+        format_string++;                                    //pointer auf das naechste Zeichen setzen
     }
-
-
 }
 
 void c_entry() {
-    volatile char * const STATUS_ADDR = (char *) DEBUG_REG + STATUS_OFF;
-    volatile char * const RECEIV_ADDR = (char *) DEBUG_REG + RECEIV_OFF;
-
     while (1) {
         if (*STATUS_ADDR & 0x01){
             my_print_f("You pressed: %c \n", RECEIV_ADDR[0]);
