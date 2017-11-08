e1cf06cca14c9f8f78d0206aa8a1924c85a50120
Jakob Krause <jakobkrause.mail@gmail.com>
2017-11-09 00:10:13 +0100

    format output

--- ab1/main.c
+++ ab1/main.c
@@ -2,60 +2,46 @@
 #define WRITE_OFF 28
 #define STATUS_OFF 20
 #define RECEIV_OFF 24
-#define MAX_STRING_LENGTH 256
-
-#define ASCII_NULL_CHAR 48
 #define INT_BYTE_COUNT 4
+
 #include <stdarg.h>
 
-
-static inline void write_u32 (unsigned int addr, unsigned int    val  ) {
-    *(volatile unsigned int   *)addr = val ;
-}
-
-static inline void print_c_pointer(const char *c){
-    //write_u32(DEBUG_REG,c); // oder komplizierter ?
-
-    volatile char * const UART0DR = (char *) DEBUG_REG;
-    *UART0DR = (unsigned int)(*c); /* Transmit char */
-}
 
 static inline void print_c(char c){
     volatile unsigned char * UART0DR = (unsigned int *) DEBUG_REG;
     UART0DR = UART0DR + WRITE_OFF;
-    *UART0DR = c; /* Transmit char */
+    *UART0DR = c; // schreibe char
 }
 
 
 static inline void print_s(const char *s){
-    while(*s != '\0') { /* Loop until end of string */
+    while(*s != '\0') { // Iteriere durch den String
         print_c(*s);
-        s++; /* Next char */
+        s++; // naechster Buchstabe
     }
 }
 
 static inline void print_x_help(char b){
     char c = '?'; // default
 
-    if( b<10){ c = '0' +b;} 	// Zahl [0-9] -> [0-9]
-    if(10<= b && b<16){ c = 'A' +b -10 ;}	// Buchstabe [10-15] -> [A-F]
+    if( b<10){
+        c = '0' +b; // Zahl [0-9] -> [0-9]
+    }
+    if(10<= b && b<16){
+        c = 'A' +b -10; // Buchstabe [10-15] -> [A-F]
+    }
     print_c(c);
-
 }
 
 static inline void print_x(unsigned int x){
-    int i; // komischer c standart
+    int i;
     int bendian[] = {0,1,2,3};
-    //int lendian[] = {3,2,1,0};
-    //int mendian[] = {2,1,4,3};
 
     int *endianOrder = bendian; // unterschiedliche Anordnung der Bytes
-
     char* pre= "0x"; // fancy
     print_s(pre);
 
     char* p = (char*) &x; // int = 4 bytes .... byte array
-
     char b= 0;
 
     for(i=0; i<INT_BYTE_COUNT; i++){
@@ -65,8 +51,6 @@ static inline void print_x(unsigned int x){
         b = (p[endianOrder[i]]) & 0x0F;	// nimm hintere 4 Bits
         print_x_help(b);
     }
-
-
 }
 
 static inline void print_p(void* p){
@@ -85,7 +69,6 @@ static inline int count_params_from_format_string(const char* s){
 
 
 void my_print_f(const char *s,...) {
-    // initialisieren der dyn Argument liste
     va_list args;
     int num_args = count_params_from_format_string(s); // N der Argumente notwendig
     va_start(args,num_args); // nun kann va_arg benutzt werden
@@ -96,15 +79,22 @@ void my_print_f(const char *s,...) {
             s++; // falls dann muss nun ein Zeichen fÃ¼r das Format kommen
             switch((*s)){
                 // Nutze entsprechende Print funktion
-                case  'c': 	print_c((char)va_arg(args,int)); 			break;
-                case  's':	print_s(va_arg(args,const char*)); 	break;
-                case  'x':	print_x(va_arg(args,unsigned int)); 	break;
-                case  'p':	print_p(va_arg(args,void*)); 			break;
-                case '\0':	//print_c(&'%');
+                case  'c':
+                    print_c((char)va_arg(args,int));
+                    break;
+                case  's':
+                    print_s(va_arg(args,const char*));
+                    break;
+                case  'x':
+                    print_x(va_arg(args,unsigned int));
+                    break;
+                case  'p':
+                    print_p(va_arg(args,void*));
+                    break;
+                case '\0':
                     continue; // Error im naechsten schritt wird Print beendet
                 default:
                     // war einfach nur ein Prozent Zeichen
-                    //print_c('%');
                     print_c(*s);
             }
         }else{
@@ -117,26 +107,14 @@ void my_print_f(const char *s,...) {
 }
 
 void c_entry() {
-    // test
-    //my_print_f("Hallo %s, das ist ein %s \nZahl: %x","Welt","Test!",1023);
     volatile char * const STATUS_ADDR = (char *) DEBUG_REG + STATUS_OFF;
     volatile char * const RECEIV_ADDR = (char *) DEBUG_REG + RECEIV_OFF;
-    //volatile unsigned int * const RECEIV_ADDR_INt = (unsigned int *) RECEIV_ADDR;
 
     while (1) {
         if (*STATUS_ADDR & 0x01){
-            print_c(RECEIV_ADDR[0]);
+            my_print_f("You pressed: %c \n", RECEIV_ADDR[0]);
         }
     }
-
-    char x = 'c';
-    unsigned int i ;
-    for(i=0;i<32;i++){
-        my_print_f("Zahl: %x\n",i);
-    }
-
-    my_print_f("Pointer: %p\n",&x);
-    my_print_f("Char %c, String %s,Hex %x, Pointer %p\n",x,"Hello",0x101,&x);
 }
 
 
