abcb2bb66283d94839bf92d352aeacae0514337f
Jakob Krause <jakobkrause.mail@gmail.com>
2017-11-22 18:12:10 +0100

    copy_vector

--- ab2/main.c
+++ ab2/main.c
@@ -1,41 +1,50 @@
-#define DEBUG_REG 0xFFFFF200
+#include <stdint.h>
+#include "my_print.h"
+
 #define REMAP_REG 0xFFFFFF00 
-#define WRITE_OFF 28
-#define STATUS_OFF 20
-#define RECEIVE_OFF 24
 #define INT_BYTE_COUNT 4
 #define VECTOR_REG_START 0xFFFFF000
-
-#define IVT_RESET 	0x00000000 // RESET
-#define IVT_UNDEFINED   0x00000004 // UNDEFINED INSTRUCTION
-#define IVT_SOFTWARE	0x00000008 // SOFTWARE INTERRUPT
-#define IVT_PREFETCH 	0x0000000C // PREFETCH ABORT
-#define IVT_DATA	0x00000010 // DATA ABORT
-#define IVT_IRQ		0x00000018 // IRQ
-#define IVT_FIQ 	0x0000001C // FIQ evtl direkt behandlen danach ist
-
-#include <stdarg.h>
+#define INTERRUPT_ENABLE 0xFFFFF120
 
 
-volatile char *const TRANS_ADDR = (char *) DEBUG_REG + WRITE_OFF;
-volatile char *const STATUS_ADDR = (char *) DEBUG_REG + STATUS_OFF;
-volatile char *const RECEIV_ADDR = (char *) DEBUG_REG + RECEIVE_OFF;
+extern volatile char *const TRANS_ADDR;
+extern volatile char *const STATUS_ADDR;
+extern volatile char *const RECEIV_ADDR;
 
 volatile char *const INTR_ADDR = (char *) VECTOR_REG_START;
+volatile char *const INTR_ENABLE = (char *) INTERRUPT_ENABLE;
 
 static inline void print_c(char c) {
     *TRANS_ADDR = c;                        // schreibe char
 }
 
-void irq_handler(){
+void __attribute__((interrupt)) irq_handler() {
     print_c('x');
 }
 
-void c_entry() {
-    int i;
-    for (i=0; i<32; i++){
-        *(INTR_ADDR+i*4) = irq_handler;
-    }
+/* all other handlers are infinite loops */
+void __attribute__((interrupt)) undef_handler(void) { for(;;); }
+void __attribute__((interrupt)) swi_handler(void) { for(;;); }
+void __attribute__((interrupt)) prefetch_abort_handler(void) { for(;;); }
+void __attribute__((interrupt)) data_abort_handler(void) { for(;;); }
+void __attribute__((interrupt)) fiq_handler(void) { for(;;); }
+
+void copy_vectors(void) {
+    extern uint32_t vectors_start;
+    extern uint32_t vectors_end;
+    uint32_t *vectors_src = &vectors_start;
+    uint32_t *vectors_dst = (uint32_t *) 0;
+    my_print_f("c");
+    my_print_f("Source Pointer: %p",&vectors_start);
+    my_print_f("DestPointer Pointer: %p",&vectors_end);
+
+    while(vectors_src < &vectors_end)
+        *vectors_dst++ = *vectors_src++;
+    print_c('p');
+}
+
+void main() {
+    *INTR_ENABLE=0;
 
     for (;;) {
         if (*STATUS_ADDR & 0x01) {                           // pruefe ob es eine neue Eingabe gab
--- ab2/startup.s
+++ ab2/startup.s
@@ -2,13 +2,6 @@
 .global _start
 _start:
     B Reset_Handler
-    B . /* Undefined */
-    B . /* SWI */
-    B . /* Prefetch Abort */
-    B . /* Data Abort */
-    B . /* reserved */
-    ldr pc,[pc,#-0xF20]
-    B .
 
 Reset_Handler:
   BL c_entry
