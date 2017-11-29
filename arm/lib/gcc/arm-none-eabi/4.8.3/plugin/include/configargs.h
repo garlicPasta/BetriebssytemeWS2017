33aa3ed57bc1189499b229d1aac8e47cd7d45da1
Robert Zifrid <robertzifrid@peking.imp.fu-berlin.de>
2017-11-29 17:16:43 +0100

    arm dir

---
+++ arm/lib/gcc/arm-none-eabi/4.8.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc-4.8.3/configure --target=arm-none-eabi --prefix=/home/mi/linnert/arm --enable-interwork --enable-languages=c --with-newlib --without-headers --disable-nls --with-mpc-include=/home/mi/linnert/toolchain/mpc-1.0.2/src --with-mpc-lib=/home/mi/linnert/toolchain/mpc-1.0.2/src/.libs --with-mpfr-lib=/home/mi/linnert/toolchain/mpfr-3.1.2/src/.libs/ --with-mpfr-lib=/home/mi/linnert/toolchain/gmp-6.0.0a/.libs";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
