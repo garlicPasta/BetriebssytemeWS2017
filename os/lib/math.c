1109824bfd6a050a8aab7617545c60d7cf5993cb
Jakob Krause <jakobkrause.mail@gmail.com>
2017-12-14 16:53:14 +0100

    added math.c

---
+++ os/lib/math.c
@@ -0,0 +1,15 @@
+int fibo(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+    else if (num == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return(fibo(num - 1) + fibo(num - 2));
+    }
+}
